31b5159e7f7ffebdc4465f42c307bf003f7fb689
Lakshya Garg <lgarg@asu.edu>
2020-01-26 13:00:06 -0700

    trial changes

--- calc.py
+++ calc.py
@@ -5,7 +5,7 @@ def subtract(x, y):
    return x - y
 # This function multiplies two numbers
 def multiply(x, y):
-   return x * y
+   return x + y
 # This function divides two numbers
 def divide(x, y):
    return x * y
